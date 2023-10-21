01a97b2f982899d0e5709867357a89d6cc42114b
hseong3243 <hseong3243@gmail.com>
2023-10-21 16:23:49 +0900

    feat: 회원 엔티티 추가

---
+++ src/main/java/com/example/member/Member.java
@@ -0,0 +1,27 @@
+package com.example.member;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    private String username;
+    private String password;
+
+    protected Member() {
+    }
+
+    public Member(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
